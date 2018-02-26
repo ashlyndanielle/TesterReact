f7bcf3fe88b5f4894035d2a92edc6b4cfa1ccb20
Ashlyn Mitros <ashlyndmitros@gmail.com>
2018-02-26 13:52:02 -0700

    react-router-dom lesson

---
+++ src/Store.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Todo from './Todo/Todo';
+
+class Store extends Component {
+  render() {
+    return (
+      <div className='main-container'>
+        Store Component
+      </div>
+    );
+  }
+}
+
+export default Store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './index.css';
+import './App.css';
+
 import App from './App';
+import About from './About';
+import Store from './Store';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={ App }/>
+      <Route path="/about" component={ About }/>
+      <Route path="/store" component={ Store }/>
+    </Switch>
+  </Router>,
+  document.getElementById('root'));
 registerServiceWorker();
